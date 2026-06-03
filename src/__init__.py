3dce573039844c7d0a67182ed43f1ed2f5670f1c
Rolando H Ramirez Jr <ramirezrolando222222@gmail.com>
2026-06-03 18:38:30 -0500

    Initialize src package

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+BrowserAi - AI-powered security-focused Internet search engine
+Romux 5 OS integration for threat detection and secure browsing
+"""
+
+__version__ = "1.0.0"
+__author__ = "Rolando Ramirez"
